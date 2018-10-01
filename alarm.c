4c5bc36c758e917a5242e7ca27c9d694732dec81
lyjohn0921 <qq879890967@gmail.com>
2018-10-01 10:37:18 -0400

    done

--- alarm.c
+++ alarm.c
@@ -6,33 +6,23 @@ int main(int argc, char const *argv[])
 {
     int i;
     if (wiringPiSetup() != -1){
-        pinMode(2, OUTPUT);
-    
-        
-    
+        pinMode(2, OUTPUT);           
         pinMode(0, INPUT);
         pinMode(1, OUTPUT);
         while(1) {
             printf("Waiting for reset\n");        
-            while(digitalRead(0) == 1)
+            while(digitalRead(0) == 1){
                 digitalWrite (1, HIGH) ;
-            
-            printf("Alarm\n");
+		ifttt("http://red.eecs.yorku.ca:8080/trigger/event/with/key/123", "my1", "my 2", "my 33333");
+		}
+            printf("Waiting for event\n");
             while(digitalRead(0) == 0){
-                digitalWrite (1, LOW) ;
-                digitalWrite (2, HIGH) ; delay (1000) ;
-                digitalWrite (2, LOW) ; delay (500) ;
+                digitalWrite (1, LOW);
+		digitalWrite (2, HIGH) ; delay (300) ;
+                digitalWrite (2, LOW) ; delay (300) ;
             }
                 
-            printf("Alarm\n");
-            ifttt("http://red.eecs.yorku.ca:8080/trigger/event/with/key/123", "my1", "my 2", "my 33333");
-
-        
-        
-        printf("Waiting for event\n");
-        digitalWrite (1, LOW) ;
-        
-        
+            printf("Alarm\n");        
         }
     }
     return 0;
